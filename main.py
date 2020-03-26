d49aea47bc694bb5f6729cb169f4d445fe735a82
NicholasP9480 <nickp9480@gmail.com>
2020-03-26 01:15:56 +0000

    added main.py file, with background color, and cat image

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-game
+# iport packages
+import pygame, random
+from pygame.locals import*
+
+# init game
+pygame.init()
+
+#initialize game
+screen_info = pygame.display.Info()
+size = (width, height) = (800, 600)
+screen = pygame.display.set_mode(size)
+
+#initialize clock
+clock = pygame.time.Clock()
+
+#initialize a background color
+color = (128, 0, 0)
+
+def main():
+    while True:
+        clock.tick(60)
+        screen.fill(color)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+  main()
